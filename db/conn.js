fce56a6f935e5fc71be08192e78b9ac61051fea2
Karllos <Karllosadriano.ofi@gmail.com>
2024-01-30 21:46:32 -0300

    epicoF

--- db/conn.js
+++ db/conn.js
@@ -2,9 +2,9 @@ const knex = require('knex');
 
 const configuration = require('../knexfile');
 
-const evironment = process.env.NODE_ENV || 'development'
+const environment = process.env.NODE_ENV || 'development'
 
 
-const conn = knex(configuration.development[environment]);
+const conn = knex(configuration[environment]);
 
 module.exports = conn;
